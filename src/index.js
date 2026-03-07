0747a9e20d702e00f9c396edbfbd19dc4f3c6192
masteraux101 <768471488@qq.com>
2026-03-07 23:52:06 +0800

    fix: convert src/index.js to CommonJS syntax

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-// The worker will replace this with the corrected CommonJS code after reading the original file.
+const { StateGraph } = require("@langchain/langgraph");
+
+// This is a placeholder for the agent state
+const agentState = {
+  messages: {
+    value: (x, y) => x.concat(y),
+    default: () => [],
+  },
+};
+
+// This is a placeholder for the graph
+const workflow = new StateGraph({
+  channels: agentState,
+});
+
+// TODO: Define nodes and edges for the graph
+
+module.exports = workflow;
